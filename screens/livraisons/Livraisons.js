3472f544f47ae044875ebfc43949e6f12c550928
oracle2_dev <mac.n.drix@gmail.com>
2022-09-22 00:50:23 +0000

    creation des pages settings, livraison et produits

---
+++ screens/livraisons/Livraisons.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Livraisons = () => {
+  return (
+    <View>
+      <Text>Livraisons</Text>
+    </View>
+  )
+}
+
+export default Livraisons
+
+const styles = StyleSheet.create({})
---
+++ screens/produits/Produits.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Produits = () => {
+  return (
+    <View>
+      <Text>Produits</Text>
+    </View>
+  )
+}
+
+export default Produits
+
+const styles = StyleSheet.create({})
