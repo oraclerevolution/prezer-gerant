de461f8a56d52dbb59ba90bd3a8bfd4f861d5a28
oracle2_dev <mac.n.drix@gmail.com>
2022-09-22 00:48:08 +0000

    fichier .js des constants

---
+++ constants/Colors.js
@@ -0,0 +1,7 @@
+const pink = "#FFECE9";
+const blue = "#233567";
+const lightblue = "#315A95";
+const palewhite = "#F6F6F6";
+const deepblue = "#1F1B34";
+
+export { pink, blue, lightblue, palewhite, deepblue };
